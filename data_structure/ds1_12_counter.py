421e8729f8accff2d0bb2f94f79522c3b83a973f
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-04-16 17:10:27 -0400

    counter
    for hashable elements

---
+++ data_structure/ds1_12_counter.py
@@ -0,0 +1,25 @@
+import collections
+
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+    'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+    'my', 'eyes', "you're", 'under'
+]
+word_counts = collections.Counter(words)
+print(word_counts)  # {'eyes': 8, 'the': 5, ...
+top_three = word_counts.most_common(3)
+print(top_three)  # [('eyes', 8), ('the', 5), ('look', 4)]
+
+# adding
+morewords = ['why', 'are', 'you', 'not', 'looking', 'in', 'my', 'eyes']
+for word in morewords:
+    word_counts[word] += 1
+# equals to
+word_counts.update(morewords)
+
+# can use arithmetic
+a = Counter(words)
+b = Counter(morewords)
+c = a + b
+d = a - b
