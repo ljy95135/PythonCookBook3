2473597405d4da443f110d4bdb5dd0c28d9ff362
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-04-16 17:46:40 -0400

    groupby
    itertools.groupby or defaultdict

---
+++ data_structure/ds1_15_group_record.py
@@ -0,0 +1,32 @@
+# groupby
+# itertools.groupby
+# must be soted at first
+import itertools
+import operator
+import collections
+
+rows = [
+    {'address': '5412 N CLARK', 'date': '07/01/2012'},
+    {'address': '5148 N CLARK', 'date': '07/04/2012'},
+    {'address': '5800 E 58TH', 'date': '07/02/2012'},
+    {'address': '2122 N CLARK', 'date': '07/03/2012'},
+    {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+    {'address': '1060 W ADDISON', 'date': '07/02/2012'},
+    {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+    {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+]
+
+rows.sort(key=operator.itemgetter('date'))
+for date, items in itertools.groupby(rows, key=operator.itemgetter('date')):
+    print(date)
+    for i in items:
+        print(' ', i)
+
+
+# use more memory (dict) but faster (no sort)
+rows_by_date = collections.defaultdict(list)
+for row in rows:
+    rows_by_date[row['date']].append(row)
+
+for r in rows_by_date['07/01/2012']:
+    print(r)
